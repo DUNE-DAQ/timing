31f5d77fb91b094f497445876c78fcac74a01dcd
Alessandro Thea <alessandro.thea@cern.ch>
2018-01-10 10:40:48 +0100

    Introducing the guardian

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 PACKAGES = \
 	core \
+	daemon \
 	python \
 	tests
 
---
+++ daemon/src/common/pdtguardian.cxx
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/**
+ * @brief      { function_description }
+ *
+ * @param[in]  argc  The argc
+ * @param      argv  The argv
+ *
+ * @return     { description_of_the_return_value }
+ */
+int main(int argc, char const *argv[])
+{
+    std::cout << "PDT Guardian" << std::endl;
+    /* code */
+    return 0;
+}
