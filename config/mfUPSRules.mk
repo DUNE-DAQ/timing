5db3c7753a47c3149625c0c25a3860c46ebe21f0
Philip Rodrigues <philip.rodrigues@physics.ox.ac.uk>
2018-01-12 10:45:33 +0100

    Install into $PDT_ROOT/ups/products instead of a hardcoded directory

--- config/mfUPSRules.mk
+++ config/mfUPSRules.mk
@@ -1,5 +1,5 @@
 INSTALL_SCRIPT := $(PDT_ROOT)/config/install_as_ups_package.sh
-UPS_PRODUCT_DIR ?= /nfs/home/phrodrig/protodune/timing/upsify/myproducts
+UPS_PRODUCT_DIR ?= $(PDT_ROOT)/ups/products
 UPS_QUALIFIERS ?= e14:prof:s50
 # UPS product names can't contain dashes so replace them with underscores
 PRODUCTNAME := $(subst -,_,$(PackageName))
@@ -9,6 +9,7 @@ PRODUCTNAME := $(subst -,_,$(PackageName))
 ups: _ups
 
 _ups:
+	mkdir -p $(UPS_PRODUCT_DIR)
 	$(INSTALL_SCRIPT) -p $(PRODUCTNAME) \
 	                  -v v$(PACKAGE_VER_MAJOR)_$(PACKAGE_VER_MINOR)_$(PACKAGE_VER_PATCH) \
 	                  -u $(RequiredUhalVersion) \
