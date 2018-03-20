fdc1edad09bcf5aff12c4205114fa94a70dca955
Alessandro Thea <alessandro.thea@cern.ch>
2018-03-20 21:57:11 +0100

    Defined the 4 `FakeTrigger` commands. See #7

--- python/pkg/pdt/cli/definitions.py
+++ python/pkg/pdt/cli/definitions.py
@@ -9,7 +9,10 @@ kCommandIDs = collections.OrderedDict([
     ('SpillStop', 0x3),
     ('RunStart', 0x4),
     ('RunStop', 0x5),
-    ('FakeTrig', 0x8),
+    ('FakeTrig0', 0x8),
+    ('FakeTrig1', 0x9),
+    ('FakeTrig2', 0xa),
+    ('FakeTrig3', 0xb),
 ])
 
 kCommandNames = { v:k for k,v in kCommandIDs.iteritems() }
