fa87d9f5357f4b269bac5608af139337752ef4de
Alessandro Thea <alessandro.thea@cern.ch>
2018-07-30 11:12:17 +0200

    No 'i2cmux' on the TLU

--- python/pkg/pdt/cli/master.py
+++ python/pkg/pdt/cli/master.py
@@ -202,19 +202,24 @@ def reset(obj, soft, fanout):
         
         # PLL and I@C reset 
         lDevice.getNode('io.csr.ctrl.pll_rst').write(0x1)
-        if lBoardType in [kBoardPC059, kBoardTLU]:
+        if lBoardType == kBoardPC059:
             lDevice.getNode('io.csr.ctrl.rst_i2c').write(0x1)
             lDevice.getNode('io.csr.ctrl.rst_i2cmux').write(0x1)
-
-        if lBoardType == kBoardPC059:
             lDevice.getNode('io.csr.ctrl.master_src').write(fanout)
+
+        elif lBoardType == kBoardTLU:
+            lDevice.getNode('io.csr.ctrl.rst_i2c').write(0x1)
 
         lDevice.dispatch()
 
         lDevice.getNode('io.csr.ctrl.pll_rst').write(0x0)
-        if lBoardType in [kBoardPC059, kBoardTLU]:
+        if lBoardType in kBoardPC059:
             lDevice.getNode('io.csr.ctrl.rst_i2c').write(0x0)
             lDevice.getNode('io.csr.ctrl.rst_i2cmux').write(0x0)
+        
+        elif lBoardType in kBoardTLU:
+            lDevice.getNode('io.csr.ctrl.rst_i2c').write(0x0)
+
         lDevice.dispatch()
 
 
