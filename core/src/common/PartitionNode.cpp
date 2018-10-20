bd167d802c55d9245a6981626d2b6d5d854219d5
Alessandro Thea <alessandro.thea@cern.ch>
2018-10-20 16:39:31 +0200

    Bugfix in Partition::stop timeout logic

--- core/src/common/PartitionNode.cpp
+++ core/src/common/PartitionNode.cpp
@@ -213,7 +213,7 @@ PartitionNode::stop( uint32_t aTimeout /*milliseconds*/ ) const {
         if ( !lInRun ) break;
 
         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-        if ( (end - start).count() > aTimeout ) {
+        if ( (end - start).count() > std::chrono::milliseconds(aTimeout) ) {
             std::ostringstream lMsg;
             lMsg << "Failed to stop after " << aTimeout << " milliseconds";
             throw RunRequestTimeoutExpired(lMsg.str());
