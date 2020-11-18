0d02ff87e1326b60d51a04d97ddfb519fbad12d7
Stoyan Trilov <stoyan.trilov@cern.ch>
2020-11-18 15:52:05 +0000

    update endpoint states map to be in sync with firmware

--- python/pkg/pdt/cli/definitions.py
+++ python/pkg/pdt/cli/definitions.py
@@ -76,18 +76,18 @@ kCarrierNamelMap = {
 
 # -----------------
 kEpStates = collections.OrderedDict([
-        ( 0x0, 'Standing by')                           , # 0b0000 when W_RST, -- Starting state after reset
-        ( 0x1, 'Waiting SFP for signal')                , # 0b0001 when W_SFP, -- Waiting for SFP LOS to go low
-        ( 0x2, 'Waiting CDR lock')                      , # 0b0010 when W_CDR, -- Waiting for CDR lock
-        ( 0x3, 'Waiting for good frequency check')      , # 0b0011 when W_FREQ, -- Waiting for good frequency check
-        ( 0x4, 'Waiting for comma alignment')           , # 0b0100 when W_ALIGN, -- Waiting for comma alignment           , stable 50MHz phase
-        ( 0x5, 'Waiting for 8b10 decoder good packet')  , # 0b0101 when W_LOCK, -- Waiting for 8b10 decoder good packet
-        ( 0x6, 'Waiting for phase adjustment command')  , # 0b0110 when W_PHASE, -- Waiting for phase adjustment command
-        ( 0x7, 'Waiting for time stamp initialisation') , # 0b0111 when W_RDY, -- Waiting for time stamp initialisation
-        ( 0x8, 'Ready')                                 , # 0b1000 when RUN, -- Good to go
-        ( 0xc, 'Error in Rx')                           , # 0b1100 when ERR_R, -- Error in rx
-        ( 0xd, 'Error in time stamp check')             , # 0b1101 when ERR_T; -- Error in time stamp check
-        ( 0xe, 'Error in physical layer after lock')    , # 0b1110 when ERR_P; -- Physical layer error after lock)
+        ( 0x0, 'Standing by')                              , # 0b0000 when W_RST,  -- Starting state after reset
+        ( 0x1, 'Waiting for SFP signal')                   , # 0b0001 when W_LINK, -- Waiting for SFP LOS to go low
+        ( 0x2, 'Waiting for good frequency check')         , # 0b0010 when W_FREQ, -- Waiting for good frequency check
+        ( 0x3, 'Waiting for phase adjustment to complete') , # 0b0011 when W_ADJUST, -- Waiting for phase adjustment to complete
+        ( 0x4, 'Waiting for comma alignment')              , # 0b0100 when W_ALIGN, -- Waiting for comma alignment, stable 50MHz phase
+        ( 0x5, 'Waiting for 8b10 decoder good packet')     , # 0b0101 when W_LOCK, -- Waiting for 8b10 decoder good packet
+        ( 0x6, 'Waiting for phase adjustment command')     , # 0b0110 when W_PHASE, -- Waiting for phase adjustment command
+        ( 0x7, 'Waiting for time stamp initialisation')    , # 0b0111 when W_RDY, -- Waiting for time stamp initialisation
+        ( 0x8, 'Ready')                                    , # 0b1000 when RUN, -- Good to go
+        ( 0xc, 'Error in Rx')                              , # 0b1100 when ERR_R, -- Error in rx
+        ( 0xd, 'Error in time stamp check')                , # 0b1101 when ERR_T, -- Error in time stamp check
+        ( 0xe, 'Error in physical layer after lock')       , # 0b1110 when ERR_P, -- Physical layer error after lock)
     ])
 
 def fmtEpState(aState):
