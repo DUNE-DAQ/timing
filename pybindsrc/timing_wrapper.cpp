324d4f5dcb600a7576805794c807fbdca1324070
Stoyan Trilov <stoyan.trilov@cern.ch>
2021-08-20 13:26:44 +0100

    remove extraneous function

--- pybindsrc/timing_wrapper.cpp
+++ pybindsrc/timing_wrapper.cpp
@@ -13,16 +13,6 @@ namespace py = pybind11;
 
 namespace dunedaq {
 namespace timing {
-
-std::map<std::string, uint32_t> swap_commands_map(const std::map<uint32_t, std::string>& command_map) {
-	std::map<std::string, uint32_t> swapped_map;
-
-	for (auto it=command_map.begin(); it != command_map.end(); ++it) {
-		swapped_map.emplace( std::pair<std::string,uint32_t>(it->second, it->first) );
-	}
-	return swapped_map;
-}
-
 namespace python {
 	
 	extern void register_i2c(py::module &);
@@ -31,7 +21,7 @@ namespace python {
 	extern void register_io(py::module &);
 	extern void register_master(py::module &);
 	extern void register_top_designs(py::module &);
-    extern void register_definitions(py::module &);
+        extern void register_definitions(py::module &);
 
 PYBIND11_MODULE(_daq_timing_py, top_module) {
 
@@ -39,19 +29,19 @@ PYBIND11_MODULE(_daq_timing_py, top_module) {
 
     // timing.core
     py::module_ core_module = top_module.def_submodule("core");
-
-	timing::python::register_i2c(core_module);
-	timing::python::register_partition(core_module);
-	timing::python::register_endpoint(core_module);
-	timing::python::register_io(core_module);
-	timing::python::register_master(core_module);
-	timing::python::register_top_designs(core_module);
-
-	// timing.common
-	py::module_ common_module = top_module.def_submodule("common");
-
-	// timing.common.definitions
-	py::module_ defs_module = common_module.def_submodule("definitions");
+    
+    timing::python::register_i2c(core_module);
+    timing::python::register_partition(core_module);
+    timing::python::register_endpoint(core_module);
+    timing::python::register_io(core_module);
+    timing::python::register_master(core_module);
+    timing::python::register_top_designs(core_module);
+    
+    // timing.common
+    py::module_ common_module = top_module.def_submodule("common");
+    
+    // timing.common.definitions
+    py::module_ defs_module = common_module.def_submodule("definitions");
     timing::python::register_definitions(defs_module);
 }
 
