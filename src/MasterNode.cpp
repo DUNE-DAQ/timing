6714700358a13f8b253aafc1b7080b81c8f5260a
Stoyan Trilov <stoyan.trilov@cern.ch>
2022-08-08 16:30:17 +0100

    send set delay, then resync in apply_delay

--- src/MasterNode.cpp
+++ src/MasterNode.cpp
@@ -214,12 +214,12 @@ MasterNode::apply_endpoint_delay(uint32_t address,      // NOLINT(build/unsigned
                                         // packet to reset rx
                                         (0x1 << 7UL) | 0x70, // write transaction on 0x70
                                         (address_mode << 7UL) | 0x1, // transaction length of 0x1
-                                        0x4,
+                                        0x3,
 
                                         // packet to set deskew done
                                         (0x1 << 7UL) | 0x70, // write transaction on 0x70
                                         (address_mode << 7UL) | 0x1, // transaction length of 0x1
-                                        0x3,
+                                        0x4,
                                     };
 
   tx_packet.back() = tx_packet.back() | (0x1 << 8UL);
