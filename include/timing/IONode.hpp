46d275e0ab0c01a61e2fa8fa402194a455b7f28e
Diana Antic <jr19588@kipper.phy.bris.ac.uk>
2023-11-17 13:05:39 +0000

    declaring pc069b

--- include/timing/IONode.hpp
+++ include/timing/IONode.hpp
@@ -222,7 +222,7 @@ protected:
                                                                 { kFMCRev1, "kFMCRev1" },     { kFMCRev2, "kFMCRev2" }, { kFMCRev3, "kFMCRev3" },
                                                                 { kPC059Rev1, "kPC059Rev1" }, { kTLURev1, "kTLURev1" }, { kSIMRev1, "kSIMRev1" },
                                                                 { kFIBRev1, "kFIBRev1" }, { kFMCRev4, "kFMCRev4" }, { kMIBRev1, "kMIBRev1" },
-                                                                { kPC069a, "pc069a" }, { kMIBRev2, "kMIBRev2" }, { kGIBRev1, "kGIBRev1" },
+                                                                { kPC069a, "pc069a" }, { kPC069b, "pc069b" }, { kMIBRev2, "kMIBRev2" }, { kGIBRev1, "kGIBRev1" },
                                                                 { kBoardRevisionUnknown, "unknown" }
   };
 
