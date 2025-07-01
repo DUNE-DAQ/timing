b942573eadc3e5237310e2b0a0ca9a5adc09647e
Stoyan Trilov <stoyan.trilov@cern.ch>
2025-07-01 15:36:02 +0100

    board temp in fib v2 io status

--- src/FIBV2IONode.cpp
+++ src/FIBV2IONode.cpp
@@ -55,6 +55,8 @@ FIBV2IONode::get_status(bool print_out) const
 	auto subnodes = read_sub_nodes(getNode("csr.stat"));
 	status << format_reg_table(subnodes, "FIB IO state") << std::endl;
 
+	status << "Board temperature: " << read_board_temperature() << " [C]" << std::endl;
+
 	if (print_out)
 	  TLOG() << status.str();
 
