eb9c0cbab2f0e7d61841adec04096f100e2f6752
Dennis Lindebaum <dennis.lindebaum@bristol.ac.uk>
2025-10-08 08:49:31 +0000

    Fix number of sfps

--- include/timing/GIBIONode.hpp
+++ include/timing/GIBIONode.hpp
@@ -136,7 +136,7 @@ public:
   float read_board_temperature() const;
 
 protected:
-  static const uint8_t num_sfps = 7;
+  static const uint8_t num_sfps = 6;
 
   static const uint8_t sfp_tx_disable_bitmap = 0xC0;
 
