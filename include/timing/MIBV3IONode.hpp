e157d453215959eb4467aa122a787853090d804b
Stoyan Trilov <stoyan.trilov@cern.ch>
2026-01-13 15:05:14 +0000

    fix v3 ionode header guards

--- include/timing/MIBV3IONode.hpp
+++ include/timing/MIBV3IONode.hpp
@@ -9,8 +9,8 @@
  * received with this code.
  */
 
-#ifndef TIMING_INCLUDE_TIMING_MIBV3IONode_HPP_
-#define TIMING_INCLUDE_TIMING_MIBV3IONode_HPP_
+#ifndef TIMING_INCLUDE_TIMING_MIBV3IONODE_HPP_
+#define TIMING_INCLUDE_TIMING_MIBV3IONODE_HPP_
 
 // PDT Headers
 #include "TimingIssues.hpp"
@@ -107,4 +107,4 @@ private:
 } // namespace timing
 } // namespace dunedaq
 
-#endif // TIMING_INCLUDE_TIMING_MIBV3IONode_HPP_
+#endif // TIMING_INCLUDE_TIMING_MIBV3IONODE_HPP_
